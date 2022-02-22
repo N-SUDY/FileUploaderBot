56a56203efe5b07072e0ef56e024268813236f1a
Sipan Group <sipantechgroup@gmail.com>
2022-02-23 00:00:55 +0330

    ّّFirst Commit

---
+++ src/main/java/FileUploaderBot.java
@@ -0,0 +1,22 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class FileUploaderBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+
+}
