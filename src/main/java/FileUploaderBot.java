0067d0fc01c33fae4667e03fade56145e5b1ea43
Sipan Group <sipantechgroup@gmail.com>
2022-02-23 13:29:57 +0330

    get file form user

--- src/main/java/FileUploaderBot.java
+++ src/main/java/FileUploaderBot.java
@@ -1,6 +1,10 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Document;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class FileUploaderBot extends TelegramLongPollingBot {
 
@@ -10,6 +14,19 @@ public class FileUploaderBot extends TelegramLongPollingBot {
         if (update.getMessage().hasDocument()){
             String fileName =update.getMessage().getDocument().getFileName();
             System.out.println(fileName);
+
+            String fileId=update.getMessage().getDocument().getFileId();
+            SendDocument sendDocument = new SendDocument();
+            sendDocument.setChatId(update.getMessage().getChatId().toString());
+            sendDocument.setDocument(new InputFile(fileId));
+            sendDocument.setCaption("upload to server...");
+
+            try {
+                execute(sendDocument);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
